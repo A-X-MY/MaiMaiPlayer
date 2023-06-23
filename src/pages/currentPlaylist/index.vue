7a73451d504d5483188a691b4c1c756c5bf1bda6
你的麦哥哥 <119749915+A-X-MY@users.noreply.github.com>
2023-06-23 21:44:00 +0800

    Update index.vue

--- src/pages/currentPlaylist/index.vue
+++ src/pages/currentPlaylist/index.vue
@@ -16,8 +16,9 @@ onMounted(() => {
 	state.tableData = musicstore.songs
 })
 
-const playerMusic = (e) => {
-	musicstore.currentIndex = e.$index
+const playerMusic = (row) => {
+	const resultIdx = musicstore.songs.findIndex(item => item.id == row.id)
+	musicstore.currentIndex = resultIdx
 }
 const rowdbClick = (row, column, cell, event) => {
 	const resultIdx = musicstore.songs.findIndex(item => item.id == row.id)
@@ -27,8 +28,9 @@ const deleteAllSongs = () => {
 	musicstore.songs.splice(1, musicstore.songs.length);
 
 }
-const deleteSong = (idx) => {
-	musicstore.songs.splice(idx.$index, 1)
+const deleteSong = (row) => {
+	const resultIdx = musicstore.songs.findIndex(item => item.id == row.id)
+	musicstore.songs.splice(resultIdx, 1)
 }
 // 添加导入本地歌曲函数
 const importMusic = () => {
@@ -60,6 +62,31 @@ const importMusic = () => {
 	}
 	input.click()
 }
+const toggleLike = (song) => {
+	musicstore.toggleLike(song)
+}
+const toggleFavorite = (song) => { // 新增的收藏功能
+	musicstore.toggleFavorite(song)
+}
+const playLiked = () => {
+		state.tableData = musicstore.songs.filter(song => song.liked)
+}
+const playFavorite = () => {
+		state.tableData = musicstore.songs.filter(song => song.Favorite)
+}
+const AllPlaylist = () => {
+	state.tableData = musicstore.songs
+}
+// 新增方法 showPlayedSongs
+const showPlayedSongs = () => {
+	// 获取已经播放过的歌曲列表 playedSongs 并按照时间排序
+	const playedSongs = musicstore.playedSongs.sort((a, b) => new Date(b.playedAt) - new Date(a.playedAt)
+	)
+// 将排序后的结果赋值给 tableData
+	state.tableData = playedSongs
+}
+
+
 
 
 </script>
@@ -73,6 +100,19 @@ const importMusic = () => {
 			<div style="width: 50px;"></div>
 			<el-button type="primary" size="medium" @click="importMusic">
 				上传歌曲
+			</el-button>
+			<!-- 播放我喜欢 -->
+			<el-button type="primary" size="medium" @click="playLiked">
+				我喜欢
+			</el-button>
+			<el-button type="primary" size="medium" @click="playFavorite">
+				我收藏
+			</el-button>
+			<el-button type="primary" size="medium" @click="AllPlaylist">
+				全部歌单
+			</el-button>
+			<el-button type="primary" size="medium" @click="showPlayedSongs">
+				最近播放
 			</el-button>
 		</div>
 
@@ -102,7 +142,7 @@ const importMusic = () => {
 							<!-- 播放 -->
 							<svg t="1648883741930" class="icon" viewBox="0 0 1024 1024" version="1.1"
 								xmlns="http://www.w3.org/2000/svg" p-id="2199" width="28" height="28"
-								@click="playerMusic(scope)">
+								@click="playerMusic(scope.row)">
 								<path
 									d="M512 0C230.4 0 0 230.4 0 512s230.4 512 512 512 512-230.4 512-512S793.6 0 512 0z m0 981.333333C253.866667 981.333333 42.666667 770.133333 42.666667 512S253.866667 42.666667 512 42.666667s469.333333 211.2 469.333333 469.333333-211.2 469.333333-469.333333 469.333333z"
 									p-id="2200" fill="#525B72"></path>
@@ -118,12 +158,21 @@ const importMusic = () => {
 									d="M590.3 532.7l-118 88.5c-9.8 7.3-23.6 5.3-31-4.4-2.9-3.8-4.4-8.5-4.4-13.3v-177c0-12.2 9.9-22.1 22.1-22.1 4.8 0 9.4 1.6 13.3 4.4l118 88.5c9.8 7.3 11.8 21.2 4.4 31-1.2 1.6-2.7 3.1-4.4 4.4zM247.5 205.3h530.9c48.9 0 88.5 39.6 88.5 88.5v442.4c0 48.9-39.6 88.5-88.5 88.5H247.5c-48.9 0-88.5-39.6-88.5-88.5V293.8c0-48.9 39.7-88.5 88.5-88.5z m442.4 44.3H336v530.9h353.9V249.6zM203.3 382.3h88.5V249.6h-44.2c-24.4 0-44.2 19.8-44.2 44.2v88.5z m619.3 0v-88.5c0-24.4-19.8-44.2-44.2-44.2h-44.2v132.7h88.4zM203.3 647.7v88.5c0 24.4 19.8 44.2 44.2 44.2h44.2V647.7h-88.4z m0-44.2h88.5v-177h-88.5v177z m619.3 44.2h-88.5v132.7h44.2c24.4 0 44.2-19.8 44.2-44.2v-88.5z m0-44.2v-177h-88.5v177h88.5z"
 									p-id="3157" fill="#525B72"></path>
 							</svg>
-							<svg @click="deleteSong(scope)" t="1686302672906" class="icon" viewBox="0 0 1024 1024"
+							<svg @click="deleteSong(scope.row)" t="1686302672906" class="icon" viewBox="0 0 1024 1024"
 								version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3249" width="28" height="28">
 								<path
 									d="M202.666667 256h-42.666667a32 32 0 0 1 0-64h704a32 32 0 0 1 0 64H266.666667v565.333333a53.333333 53.333333 0 0 0 53.333333 53.333334h384a53.333333 53.333333 0 0 0 53.333333-53.333334V352a32 32 0 0 1 64 0v469.333333c0 64.8-52.533333 117.333333-117.333333 117.333334H320c-64.8 0-117.333333-52.533333-117.333333-117.333334V256z m224-106.666667a32 32 0 0 1 0-64h170.666666a32 32 0 0 1 0 64H426.666667z m-32 288a32 32 0 0 1 64 0v256a32 32 0 0 1-64 0V437.333333z m170.666666 0a32 32 0 0 1 64 0v256a32 32 0 0 1-64 0V437.333333z"
 									fill="#000000" p-id="3250"></path>
 							</svg>
+							<svg viewBox="0 0 24 24" width="28" height="28" aria-hidden="true" @click.stop="toggleLike(scope.row)">
+								<path :stroke="scope.row.liked ? 'red' : 'currentColor'" :fill="scope.row.liked ? 'red' : 'none'" stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z">
+								</path>
+							</svg>
+							<svg viewBox="0 0 24 24" width="28" height="28" aria-hidden="true" @click.stop="toggleFavorite(scope.row)">
+								<path :stroke="scope.row.Favorite ? 'black' : 'currentColor'" :fill="scope.row.Favorite ? 'black' : 'none'" stroke-linecap="round" stroke-linejoin="round"  d="M12,2 L15.09,8.472 L22,9.27 L17,14.02 L18.18,21 L12,17.77 L5.82,21 L7,14.02 L2,9.27 L8.91,8.47 L12,2 Z" fill="none"/>
+							</svg>
+
+
 						</div>
 					</template>
 				</el-table-column>
