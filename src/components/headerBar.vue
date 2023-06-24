96f8875ff0f64c4ba2a34a159d3fa604cd0b9177
你的麦哥哥 <119749915+A-X-MY@users.noreply.github.com>
2023-06-24 15:29:04 +0800

    Update headerBar.vue

--- src/components/headerBar.vue
+++ src/components/headerBar.vue
@@ -115,14 +115,14 @@ const submit = () => {
 						p-id="2831" fill="#525B72"></path>
 				</svg>
 			</a>
-			<!-- <a class="menu-link is-active" href="javascript:;" @click="router.go(1)">
+			 <a class="menu-link is-active" href="javascript:;" @click="router.go(1)">
 				<svg t="1648618362955" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
 					p-id="2626" width="32" height="32">
 					<path
 						d="M448 672c-6.4 0-19.2 0-25.6-6.4-12.8-12.8-12.8-32 0-44.8L531.2 512 422.4 409.6c-12.8-12.8-12.8-32 0-44.8s32-12.8 44.8 0l128 128c12.8 12.8 12.8 32 0 44.8l-128 128C467.2 672 454.4 672 448 672z"
 						p-id="2627" fill="#525B72"></path>
 				</svg>
-			</a> -->
+			</a>
 		</div>
 
 		<div class="search-bar">
@@ -180,14 +180,10 @@ const submit = () => {
 				</template>
 			</el-dropdown>
 			<div class="username">{{ globalstore.userInfo.nickname }}</div>
-			<div class="notification">
-				<span class="notification-number">3</span>
-				<svg t="1648737299278" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-					p-id="8950" width="25" height="25">
-					<path
-						d="M192.032 631.402667V404.725333C192.032 228.330667 335.285333 85.333333 512 85.333333s319.968 142.997333 319.968 319.392v226.677334l60.608 121.013333c10.645333 21.237333-4.832 46.218667-28.618667 46.218667H160.042667c-23.786667 0-39.253333-24.981333-28.618667-46.218667l60.608-121.013333z m620.16 103.36l-40.842667-81.536a31.893333 31.893333 0 0 1-3.381333-14.282667V404.725333c0-141.12-114.602667-255.509333-255.968-255.509333S256.032 263.605333 256.032 404.725333V638.933333c0 4.96-1.162667 9.845333-3.381333 14.293334l-40.842667 81.525333h600.384z m-443.306667 152.32a31.893333 31.893333 0 0 1-4.149333-44.981334 32.032 32.032 0 0 1 45.056-4.138666A159.36 159.36 0 0 0 512 874.773333a159.36 159.36 0 0 0 102.186667-36.8 32.032 32.032 0 0 1 45.056 4.138667 31.893333 31.893333 0 0 1-4.16 44.981333A223.402667 223.402667 0 0 1 512 938.666667c-52.981333 0-103.2-18.453333-143.114667-51.594667z"
-						p-id="8951" fill="#525B72"></path>
-				</svg>
+			<div class="logo">
+				<a href="javascript:;" onclick="window.open('https://music.163.com/#/discover', '_blank')">
+					<img src="../static/img/aplayer/logo.svg" alt="" class="Musicice" style="transform: scale(1.5);" />
+				</a>
 			</div>
 		</div>
 		<!-- 已登陆 -->
